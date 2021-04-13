703eec9b62d1db099659ec3ac870d4e9d6a88248
Denys Chornyi <chornijj14@gmail.com>
2021-04-13 16:12:31 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/square_gestures_animation/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.square_gestures_animation
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Gestures and Animations',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int numTaps = 0;
+  int numDoubleTaps = 0;
+  int numLongPress = 0;
+
+  double posX = 0.0;
+  double posY = 0.0;
+  double boxSize = 150.0;
+
+  @override
+  Widget build(BuildContext context) {
+    if(posX == 0.0){
+      center(context);
+    }
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Geastures and Animation"),
+      ),
+      body: GestureDetector(
+        onTap: (){
+          setState(() {
+            numTaps += 1;
+          });
+        },
+        onDoubleTap: (){
+          setState(() {
+            numDoubleTaps += 1;
+          });
+        },
+        onLongPress: (){
+          setState(() {
+            numLongPress += 1;
+          });
+        },
+        onVerticalDragUpdate: (DragUpdateDetails value){
+          setState(() {
+            double delta = value.delta.dy;
+            posY += delta;
+          });
+        },
+        onHorizontalDragUpdate: (DragUpdateDetails value){
+          setState(() {
+            double delta = value.delta.dx;
+            posX +=delta;
+          });
+        },
+        child: Stack(
+          children: [
+            Positioned(
+                left: posX,
+                top: posY,
+                child: Container(
+                  width: boxSize,
+                  height: boxSize,
+                  decoration: BoxDecoration(color: Colors.red),
+                )),
+          ],
+        ),
+      ),
+      bottomNavigationBar: Material(
+        color: Theme.of(context).primaryColorLight,
+        child: Padding(
+          padding: EdgeInsets.all(15),
+          child: Text(
+            "Taps: $numTaps - Double Taps: $numDoubleTaps - Long Presses: $numLongPress",
+            style: Theme.of(context).textTheme.headline6,
+          ),
+        ),
+      ),
+    );
+  }
+
+  void center(BuildContext context) {
+    posX = (MediaQuery.of(context).size.width / 2) - boxSize / 2;
+    posY = (MediaQuery.of(context).size.height / 2) - boxSize / 2 - 30.0;
+
+    setState(() {
+      posX = posX;
+      posY = posY;
+    });
+  }
+}
